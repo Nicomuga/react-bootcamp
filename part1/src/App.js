b9cf8d746427b13880f24609356604c08b231f51
Muga <nmugasaez@gmail.com>
2023-01-03 12:05:23 -0300

    part 1

---
+++ part1/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Mensaje from './Mensaje.js'
+
+
+const Description = () => {
+  return <p> Esta es una app del curso fullstack bootcamp</p>
+}
+
+const App = () => {
+
+  
+  return ( 
+    <div className="App">
+      <Mensaje color='red' message = 'Estamos trabajando'/> 
+      <Mensaje color='green' message = 'En un curso'/>
+      <Mensaje color='yellow' message = 'De react' />
+      <Description />
+    </div>
+  );
+}
+
+export default App;
